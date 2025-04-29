ce6f3ab0324a2857e8ddb1eb62fd9ad809f493e7
scaleoka <125462447+scaleoka@users.noreply.github.com>
2025-04-29 10:33:32 +0000

    Update minersunion-parser.py

--- minersunion-parser.py
+++ minersunion-parser.py
@@ -1,6 +1,3 @@
-from dotenv import load_dotenv
-load_dotenv()            # <- прочитает .env и положит пары KEY=VALUE в os.environ
-
 import os
 import requests
 from oauth2client.service_account import ServiceAccountCredentials
